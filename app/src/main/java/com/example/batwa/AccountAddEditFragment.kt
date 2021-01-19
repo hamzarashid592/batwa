720dfdb17bfff529d3f05f38baca42062df17add
Hamza Rashid <hamzarashid592@yahoo.com>
2021-01-19 11:43:24 +0500

    Added an account add / edit fragment

---
+++ app/src/main/java/com/example/batwa/AccountAddEditFragment.kt
@@ -0,0 +1,58 @@
+package com.example.batwa
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.fragment_account_add_edit.*
+import java.text.DecimalFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class AccountAddEditFragment : Fragment() {
+
+
+    private var accountNames = ArrayList<String>()
+
+    private lateinit var dbHelper: DBHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        //Context becomes available here..
+        dbHelper = DBHelper(context, null)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_add_edit, container, false)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+}
